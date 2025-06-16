bac325dfaabd93938ed323e6f6745518f4570a5f
Leandro Felix <107886470+LeandroHawk@users.noreply.github.com>
2025-06-15 22:46:48 -0300

    Update Book.java

--- TP03 BOOKSTORE/Bookstore/src/net/codejava/javaee/bookstore/Book.java
+++ TP03 BOOKSTORE/Bookstore/src/net/codejava/javaee/bookstore/Book.java
@@ -4,7 +4,7 @@ package net.codejava.javaee.bookstore;
  * Book.java
  * This is a model class represents a book entity
  * @author www.codejava.net
- *
+ * LEANDRO FELIX E KAIK PERSIKE
  */
 public class Book {
     protected int id;
